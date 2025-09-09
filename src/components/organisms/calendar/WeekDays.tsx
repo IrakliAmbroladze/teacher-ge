71155826e697ff8a4aa33ecca541cc489f302461
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-09 20:02:52 +0400

    feat(calendar): add wee days on top

--- src/components/organisms/calendar/WeekDays.tsx
+++ src/components/organisms/calendar/WeekDays.tsx
@@ -1,6 +1,13 @@
 import { geoWeekdaysArray } from "@/constants";
 
 export default function WeekDays() {
-  console.log(geoWeekdaysArray);
-  return <div>WeekDays</div>;
+  return (
+    <>
+      {geoWeekdaysArray.map((day) => (
+        <div key={day} className="text-sm flex justify-center">
+          {day}
+        </div>
+      ))}
+    </>
+  );
 }
