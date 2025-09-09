402798a0b161e45abad2f40da4b2b2228c572db1
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-10 01:04:27 +0400

    feat(ag-grid): add AgGrid component

--- src/components/organisms/Contacts.tsx
+++ src/components/organisms/Contacts.tsx
@@ -1,22 +1,64 @@
 "use client";
 
 import type { Contact } from "@/types";
-import React from "react";
-import { AllCommunityModule, ModuleRegistry } from "ag-grid-community";
+import React, { useMemo } from "react";
+import {
+  AllCommunityModule,
+  ColDef,
+  ICellRendererParams,
+  ModuleRegistry,
+} from "ag-grid-community";
+import { AgGridReact } from "ag-grid-react";
+import { HEADER_HEIGHT } from "@/lib/constants";
 
 ModuleRegistry.registerModules([AllCommunityModule]);
 
 export function Contacts({ contacts }: { contacts: Contact[] }) {
+  const defaultColDef = useMemo(() => {
+    return {
+      width: 150,
+      cellStyle: { fontWeight: "bold" },
+      filter: true,
+      filterParams: { buttons: ["clear"] },
+      floatingFilter: true,
+      editable: false,
+    };
+  }, []);
+
+  const colDefs: ColDef<Contact>[] = [
+    { field: "name" },
+    {
+      field: "phone",
+      cellRenderer: (params: ICellRendererParams<Contact>) => {
+        const phone = params.value;
+        return (
+          <a
+            href={`tel:${phone}`}
+            style={{ color: "#2563eb", textDecoration: "underline" }}
+          >
+            {phone}
+          </a>
+        );
+      },
+    },
+    {
+      field: "role",
+
+      width: 350,
+    },
+    { field: "description", width: 350 },
+  ];
   return (
-    <>
-      {contacts.map((contact) => (
-        <div key={contact.id} className="flex gap-2">
-          <div>{contact.name}</div>
-          <div>{contact.phone}</div>
-          <div>{contact.role}</div>
-          <div>{contact.description}</div>
-        </div>
-      ))}
-    </>
+    <div
+      style={{ height: `calc(100dvh - ${HEADER_HEIGHT}px)` }}
+      className="p-2.5"
+    >
+      <AgGridReact
+        rowData={contacts}
+        columnDefs={colDefs}
+        defaultColDef={defaultColDef}
+        enableCellTextSelection={true}
+      />
+    </div>
   );
 }
