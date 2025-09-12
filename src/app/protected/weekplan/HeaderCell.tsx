3edb064566b390361281c2a452f8c8906652db62
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2025-09-12 15:50:25 +0400

    refac(weekplan): use only day names in grid header

--- src/app/protected/weekplan/HeaderCell.tsx
+++ src/app/protected/weekplan/HeaderCell.tsx
@@ -1,17 +1,19 @@
 import React from "react";
-import { daysArrayOfCurrentWeek } from "@/utils";
+//import { daysArrayOfCurrentWeek } from "@/utils";
+import { geoWeekdaysArray } from "@/constants";
 
 export default function HeaderCell({ index }: { index: number }) {
-  const days = daysArrayOfCurrentWeek();
-  const month = days[index].toLocaleString("ka-GE", { month: "long" });
-  const dayName = days[index].toLocaleString("ka-GE", { weekday: "long" });
-  const dayNumber = days[index].toLocaleString("ka-GE", { day: "numeric" });
+  //  const days = daysArrayOfCurrentWeek();
+  //  const month = days[index].toLocaleString("ka-GE", { month: "long" });
+  //  const dayName = days[index].toLocaleString("ka-GE", { weekday: "long" });
+  //  const dayNumber = days[index].toLocaleString("ka-GE", { day: "numeric" });
   return (
     <div>
-      <div className="text-center">{dayName}</div>
+      {geoWeekdaysArray[index]}
+      {/* <div className="text-center">{dayName}</div>
       <div className="text-center">
         {dayNumber} {month}
-      </div>
+      </div> */}
     </div>
   );
 }
