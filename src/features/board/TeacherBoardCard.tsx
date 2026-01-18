e1576ce95ec506d27c6a78da0edbf3f292b89bcd
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-18 19:34:35 +0400

    style(board-card): add shadow color stone 800 on dark mode

--- src/features/board/TeacherBoardCard.tsx
+++ src/features/board/TeacherBoardCard.tsx
@@ -5,7 +5,7 @@ type TeacherBoardCardProps = Omit<TeachersBoard, "id">;
 
 export const TeacherBoardCard = ({ name, img_url }: TeacherBoardCardProps) => {
   return (
-    <div className="p-[20px] w-[150px] h-[200px] flex flex-col items-center rounded-md shadow-2xl cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out">
+    <div className="p-[20px] w-[150px] h-[200px] flex flex-col items-center rounded-md shadow-2xl cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out dark:shadow-stone-800/80">
       <div className="relative w-full h-full mb-2">
         <Image alt={name} src={img_url} fill className="object-cover rounded" />
       </div>
