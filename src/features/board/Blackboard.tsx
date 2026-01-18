d2c2b10f28dc67587de554f9899aad3830e1a712
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-18 23:06:47 +0400

    feat(BLackboard): add buttons for editing saving and cancelling

--- src/features/board/Blackboard.tsx
+++ src/features/board/Blackboard.tsx
@@ -1,20 +1,41 @@
 "use client";
 import { Button } from "@/components";
+import { useState } from "react";
 
 export const Blackboard = ({ name }: { name: string }) => {
+  const [isEditing, setIsEditing] = useState<boolean>(false);
   return (
-    <div className="flex flex-col gap-2 p-5 h-full">
+    <div className="flex flex-col gap-2 p-5">
       <h1 className="text-4xl md:text-5xl font-extrabold tracking-tight text-gray-900 dark:text-stone-400 text-center">
         {name.toUpperCase()}
       </h1>
       <div className="flex justify-center">
-        <Button
-          textContent="edit"
-          bgColor="#27D3F5"
-          handleClick={() => {
-            console.log("hi");
-          }}
-        />
+        {isEditing ? (
+          <div className="flex gap-2">
+            <Button
+              textContent="save"
+              bgColor="#008000"
+              handleClick={() => {
+                setIsEditing((prev) => !prev);
+              }}
+            />
+            <Button
+              textContent="cancel"
+              bgColor="#C0C0C0"
+              handleClick={() => {
+                setIsEditing((prev) => !prev);
+              }}
+            />
+          </div>
+        ) : (
+          <Button
+            textContent="edit"
+            bgColor="#27D3F5"
+            handleClick={() => {
+              setIsEditing((prev) => !prev);
+            }}
+          />
+        )}
       </div>
     </div>
   );
