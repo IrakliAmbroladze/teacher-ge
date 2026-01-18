2f57d0428d20944c4effdf0f9be5eefedfa3670a
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-18 22:45:11 +0400

    feat(Button): add Button component in Blackboard

--- src/features/board/Blackboard.tsx
+++ src/features/board/Blackboard.tsx
@@ -1,9 +1,21 @@
+"use client";
+import { Button } from "@/components";
+
 export const Blackboard = ({ name }: { name: string }) => {
   return (
-    <>
+    <div className="flex flex-col gap-2 p-5 h-full">
       <h1 className="text-4xl md:text-5xl font-extrabold tracking-tight text-gray-900 dark:text-stone-400 text-center">
         {name.toUpperCase()}
       </h1>
-    </>
+      <div className="flex justify-center">
+        <Button
+          textContent="edit"
+          bgColor="#27D3F5"
+          handleClick={() => {
+            console.log("hi");
+          }}
+        />
+      </div>
+    </div>
   );
 };
