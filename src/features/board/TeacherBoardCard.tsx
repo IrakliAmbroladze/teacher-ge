b7c0683de9a57beda4293a558eebb0a58dab1f21
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-18 19:56:16 +0400

    feat(BoardCard): add routes for each class

--- src/features/board/TeacherBoardCard.tsx
+++ src/features/board/TeacherBoardCard.tsx
@@ -1,16 +1,21 @@
 import Image from "next/image";
 import { TeachersBoard } from "./types";
+import Link from "next/link";
 
-type TeacherBoardCardProps = Omit<TeachersBoard, "id">;
-
-export const TeacherBoardCard = ({ name, img_url }: TeacherBoardCardProps) => {
+export const TeacherBoardCard = ({
+  name,
+  img_url,
+  url,
+}: Omit<TeachersBoard, "id">) => {
   return (
-    <div className="p-[20px] w-[150px] h-[200px] flex flex-col items-center rounded-md shadow-2xl cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out dark:shadow-stone-800/80">
+    <Link
+      href={url}
+      className="p-[20px] w-[150px] h-[200px] flex flex-col items-center rounded-md shadow-2xl cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out dark:shadow-stone-800/80"
+    >
       <div className="relative w-full h-full mb-2">
         <Image alt={name} src={img_url} fill className="object-cover rounded" />
       </div>
-
-      <div className="text-center font-medium">{name}</div>
-    </div>
+      <h2 className="text-center font-medium">{name}</h2>
+    </Link>
   );
 };
