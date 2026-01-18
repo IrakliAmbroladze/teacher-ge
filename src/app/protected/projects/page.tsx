2636457031d0cbe8a6570cfeac69dcdd1b283649
Irakli Ambroladze <ambroladzeirakli@gmail.com>
2026-01-18 18:37:32 +0400

    style(board-card): style board cards

--- src/app/protected/projects/page.tsx
+++ src/app/protected/projects/page.tsx
@@ -3,12 +3,59 @@ import { TeachersBoard } from "@/features/board/types";
 
 export default function ProjectsPage() {
   const TEACHERS_BOARDS: TeachersBoard[] = [
-    { id: 1, name: "Irakli", img_url: "url-1" },
-    { id: 2, name: "Tikuna", img_url: "second image" },
+    {
+      id: 1,
+      name: "ეკოლოგები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 2,
+      name: "ფლამინგოები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 3,
+      name: "მაძიებლები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 4,
+      name: "ფლორისტები",
+      img_url: "/teachers/florists.jpg",
+    },
+    {
+      id: 6,
+      name: "დისნეი",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 7,
+      name: "მინიონები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 8,
+      name: "ლეგოები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 9,
+      name: "ვიტამინები",
+      img_url: "/calendar.png",
+    },
+    {
+      id: 10,
+      name: "მფრინავები",
+      img_url: "/teachers/pilots.png",
+    },
+    {
+      id: 11,
+      name: "საერთო",
+      img_url: "/calendar.png",
+    },
   ];
   return (
-    <>
-      <div>This is a projects page</div>
+    <ul className="flex flex-wrap gap-2 justify-center">
       {TEACHERS_BOARDS.map((item) => {
         return (
           <li key={item.id}>
@@ -16,6 +63,6 @@ export default function ProjectsPage() {
           </li>
         );
       })}
-    </>
+    </ul>
   );
 }
--- src/features/board/TeacherBoardCard.tsx
+++ src/features/board/TeacherBoardCard.tsx
@@ -1,13 +1,16 @@
+import Image from "next/image";
 import { TeachersBoard } from "./types";
 
-export const TeacherBoardCard = ({
-  name,
-  img_url,
-}: Omit<TeachersBoard, "id">) => {
+type TeacherBoardCardProps = Omit<TeachersBoard, "id">;
+
+export const TeacherBoardCard = ({ name, img_url }: TeacherBoardCardProps) => {
   return (
-    <>
-      <div>{name}</div>
-      <div>{img_url}</div>
-    </>
+    <div className="p-[20px] w-[150px] h-[200px] flex flex-col items-center rounded-md shadow-2xl cursor-pointer hover:scale-105 transition-transform duration-150 ease-in-out">
+      <div className="relative w-full h-full mb-2">
+        <Image alt={name} src={img_url} fill className="object-cover rounded" />
+      </div>
+
+      <div className="text-center font-medium">{name}</div>
+    </div>
   );
 };
